967f4d11078b0a84c20cd64dc8b4c7931eb8d34e
Christian Obed Arana navas <COANelmejor@gmail.com>
2020-09-12 12:37:03 -0600

    Cambios en el recaptcha y recover password

--- routes/recover-password/index.js
+++ routes/recover-password/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 const send = require('./send')
 const home = require('./home')
-router.get('/', home);
 
+router.get('/', home);
 router.post('/send', send)
 
 module.exports = router;
--- routes_api_v1/users/index.js
+++ routes_api_v1/users/index.js
@@ -7,7 +7,7 @@ const recoverPassword = require('./recoverPassword')
 const isLoggedAPI = require('../../mw/isUserLoggedAPI')
 
 router.post('/register', create)
-router.post('/password', isLoggedAPI, changePassword)
+router.post('/change-password', isLoggedAPI, changePassword)
 router.post('/validate-email', validateEmail)
 router.post('/recover-password', recoverPassword)
 
