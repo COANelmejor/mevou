ddbbecc72e4d2766c9251a1d7ce0f50f66e414c9
uriel3 <uriel@grupoperinola.com>
2020-09-11 23:14:58 -0600

    nueva ruta de login

---
+++ routes/login/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+  res.render('login', {
+    title: 'Bienvenidos a Μενού',
+    user: req.user
+  });
+});
+
+module.exports = router;
