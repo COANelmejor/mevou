5a35294f5178157ae0b69acbdcc6e5b5affbc2e7
Christian Obed Arana navas <COANelmejor@gmail.com>
2020-09-12 14:10:20 -0600

    agregadas tareas a api de users

--- routes_api_v1/users/index.js
+++ routes_api_v1/users/index.js
@@ -5,6 +5,7 @@ const changePassword = require('./passwordChange')
 const validateEmail = require('./validateEmail')
 const recoverPassword = require('./recoverPassword')
 const recoverPasswordChange = require('./recoverPasswordChange')
+// const myProfile = require('./myProfile')
 const isLoggedAPI = require('../../mw/isUserLoggedAPI')
 
 router.post('/register', create)
@@ -12,5 +13,16 @@ router.post('/change-password', isLoggedAPI, changePassword)
 router.post('/validate-email', validateEmail)
 router.post('/recover-password', recoverPassword)
 router.post('/recover-password-change', recoverPasswordChange)
+// router.post('/my-profile', myProfile)
+
+// TODO Agregar URL de consulta de perfil personal
+// TODO Agregar los siguiente parametros para que no se puedan cambiar en el Perfil 
+// rpmt
+// rpv
+// prpc
+// rcv
+// email_verificado
+// email_verificado_timestamp
+
 
 module.exports = router
