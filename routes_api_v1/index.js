a48e5b0573cb44070f94e976b31f222a49baf2e7
Christian Obed Arana navas <COANelmejor@gmail.com>
2020-09-13 16:46:58 -0600

    Agreados /api/menus a APIs

--- routes_api_v1/index.js
+++ routes_api_v1/index.js
@@ -2,4 +2,5 @@ module.exports = function (app) {
     app.use('/api/v1', require('./home'));
     app.use('/api/v1/logout', require('./logout'));
     app.use('/api/v1/users', require('./users'));
+    app.use('/api/v1/menus', require('./menus'));
 }
