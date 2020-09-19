3475c96f103b9d3f04010540a34ca613d85a0194
Uriel Arana <udan1718@gmail.com>
2020-09-19 12:47:25 -0600

    Add static views suscripción y perfil

---
+++ routes/suscripcion/principal/index.js
@@ -0,0 +1,7 @@
+module.exports = function (req, res) {
+    res.render('suscripcion', {
+        title: 'Mi Suscripción - Μενού',
+        h1: 'Mi Suscripción',
+        h1icon: 'fas fa-credit-card'
+    });
+}
