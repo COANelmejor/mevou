9811c914f555402486762afdca63e6fa5ea2eb92
Uriel Arana <udan1718@gmail.com>
2020-09-24 06:52:11 -0600

    Vista de Menu Al Publico static integration success

--- routes/m/principal/index.js
+++ routes/m/principal/index.js
@@ -1,7 +1,4 @@
 module.exports = function (req, res) {
-    res.render('dashboard', {
-        title: 'Dashboard - Μενού',
-        h1: 'Inicio',
-        h1icon: 'fas fa-tachometer-alt'
-    });
+    res.render('m', {
+        title: 'Μενού',});
 }
