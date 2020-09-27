7b909728fc56f4aa45fdc88cbc7fd4d9e578ff56
Uriel Arana <udan1718@gmail.com>
2020-09-27 01:01:30 -0600

    Integración de vista-publica-menu 100% e integracion de dashboard crear-modificar-eliminar 100%

--- routes/m/index.js
+++ routes/m/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const home = require('./principal');
-const isLogged = require('../../mw/isLogged');
 
 /* GET home page. */
 router.get('/:shortname', home);
